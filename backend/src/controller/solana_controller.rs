ce15226a8c5dc49ffcc7042d3937d51222990566
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-29 02:05:53 +0530

    created client and change the struct

--- backend/src/controller/solana_controller.rs
+++ backend/src/controller/solana_controller.rs
@@ -1,10 +1,30 @@
 use crate::MainDataStruct;
 use actix_web::{HttpResponse, Responder, get, post, web};
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_commitment_config::CommitmentConfig;
+use solana_sdk::account_info;
+
+//create the rpc client
+// let  client = RpcClient::new_with_commitment(
+//     String::from("https://api.mainnet.solana.com"),
+//     CommitmentConfig::confirmed(),
+// );
 
 //fetch data
 #[post("/data")]
 pub async fn fetch_data(data: web::Json<MainDataStruct>) -> impl Responder {
-    let value = data.data.to_string();
-    println!("the body data is : {}", &value);
-    HttpResponse::Ok().body(value)
+    let address_array = data.data.clone();
+
+    //create the client
+    let client = RpcClient::new_with_commitment(
+        String::from("https://api.mainnet.solana.com"),
+        CommitmentConfig::confirmed(),
+    );
+
+    //for loop
+    for addr in address_array {
+        println!("addr is {}", addr);
+    }
+
+    HttpResponse::Ok().body("value is acheived")
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,11 @@
 //create the web server in this only
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 use serde::Deserialize;
+use solana_sdk::pubkey::Pubkey;
 use std::io;
 mod controller;
 mod routes;
 use controller::fetch_data;
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
 
 // pub struct AppDataStruct {
 //     data: Mutex<String>,
@@ -18,16 +14,11 @@ async fn echo(req_body: String) -> impl Responder {
 //main data struct
 #[derive(Debug, Deserialize)]
 pub struct MainDataStruct {
-    data: String,
+    data: Vec<Pubkey>,
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    //create instance of the main data struct
-    // let main_data = web::Data::new(AppDataStruct {
-    //     data: Mutex::new("data".to_string()),
-    // });
-
     //the main server instance
     HttpServer::new(move || {
         //moving main_data into the closure

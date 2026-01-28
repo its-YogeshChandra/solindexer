acd7a6650720c2cb5b9e61abb6c3b8e204a76e4f
its-YogeshChandra <pandittheroyal@gmail.com>
2026-01-28 22:43:06 +0530

    added file structure and created mod file

---
+++ backend/src/controller/solana_controller.rs
@@ -0,0 +1,8 @@
+use actix_web::{HttpResponse, Responder};
+use tokio;
+
+//fetch data 
+#[tokio::main]
+aysnc fn fetch_data(arg: Type) -> impl Responder {
+
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,8 @@
 //create the web server in this only
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
-use std::io;
+use std::{io, sync::Mutex};
+mod controller;
+mod routes;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -12,12 +14,24 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+//main data struct
+struct MainDataStruct {
+    data: Mutex<String>,
+}
+
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     println!("web server in the rust ");
+
+    //create instance of the main data struct
+    let main_data = web::Data::new(MainDataStruct {
+        data: Mutex::new("data".to_string()),
+    });
+
     HttpServer::new(|| App::new().service(hello).service(echo))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
-    //have to put the meessage that server is running on port :
+
+    //have to put the meessage that server is running on port
 }
